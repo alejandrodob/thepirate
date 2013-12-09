e3f01b9e86e4a75aff81aa519dd11c3f0f3547e0
alejandrodob <alejandrodob@gmail.com>
2013-12-09 13:49:30 +0100

    Initial commit. environment ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thepirate::Application.config.secret_key_base = '26e0fa422aea422a902a73ddce59051f9fc673e35470fdc595d329cdb1d00a3f70f25eefbe28764ce36cdd3e7190e8d184acf2e642b0f1949373b6b9591d6f72'
