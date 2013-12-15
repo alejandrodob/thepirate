2d419e47286b4495d72de6415a8497799c7829b1
alejandrodob <alejandrodob@gmail.com>
2013-12-15 14:05:15 +0100

    removed some useless files

--- spec/factories/user.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    email "a@a.a"
-    password "1111111111"
-    nickname "john doe"
-  end
-end
