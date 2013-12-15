58f48d2c29e6768332f418c1dacc0fa6174b2e59
alejandrodob <alejandrodob@gmail.com>
2013-12-15 17:13:42 +0100

    added username uniqueness validation and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :friends, through: :friendships
 
   validates_presence_of :username
+  validates_uniqueness_of :username
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,17 @@ describe User do
       expect(create(:user)).to be_valid
     end
 
-    it "presence of a username" do
+    it "has a username" do
       user = build(:user, username: nil)
       expect(user).not_to be_valid
     end
+
+    it "username is unique" do
+      user = create(:user, username: 'mindy', email:'w@w.w')
+      user2 = build(:user, username: 'mindy')
+
+      expect(user2).not_to be_valid
+    end
+
   end
 end
