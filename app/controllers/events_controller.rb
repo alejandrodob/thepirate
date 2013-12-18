04abe13bbe9e3ff167061784bea6e966375d9988
alejandrodob <alejandrodob@gmail.com>
2013-12-18 16:44:01 +0100

    fixed params for participant_ids

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -74,7 +74,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit!
+      params.require(:event).permit(:name, participant_ids: [])
     end
 
 end
